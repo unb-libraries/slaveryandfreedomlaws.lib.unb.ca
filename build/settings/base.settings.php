9e78afae0178506fe013ca2e96232724c56af1a4
Jacob Sanford <jsanford@unb.ca>
2021-03-12 09:43:20 -0400

    AASLP Create Lean Github Repository

---
+++ build/settings/base.settings.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Include global settings overrides here.
+ */
+
+// Specify install profile.
+$settings['install_profile'] = 'minimal';
+
+// Redis.
+$settings['cache_prefix']['default'] = 'aaslp_';
+$conf['chq_redis_cache_enabled'] = TRUE;
+require_once dirname(__FILE__) . "/settings.redis.inc";
+
+// Newrelic.
+if (extension_loaded('newrelic')) {
+  require_once dirname(__FILE__) . "/settings.newrelic.inc";
+}
