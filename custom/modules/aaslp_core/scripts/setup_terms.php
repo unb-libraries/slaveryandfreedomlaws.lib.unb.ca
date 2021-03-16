95c0d388eb04a42d4457a97d7dbead45272c2517
cvillami <cvillami@unb.ca>
2021-03-16 11:51:35 -0300

    AASLP-83 Enable views_ui + block/theme de-clutter

--- custom/modules/aaslp_core/scripts/setup_terms.php
+++ custom/modules/aaslp_core/scripts/setup_terms.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
-* @file
-* Contains setup_terms.php.
-*
-* Sets/updates taxonomy term vocabularies for aaslp.lib.unb.ca
-*/
+ * @file
+ * Contains setup_terms.php.
+ *
+ * Sets/updates taxonomy term vocabularies for aaslp.lib.unb.ca.
+ */
 
 use Drupal\taxonomy\Entity\Term;
