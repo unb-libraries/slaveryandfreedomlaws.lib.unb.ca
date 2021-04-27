eb2b0b4b6282fe5d3e336fe966c98cc22a278877
cvillami <cvillami@unb.ca>
2021-04-27 15:03:18 -0300

    AASLP-119 + Utility script clr_nodes and increase test article amount

---
+++ custom/modules/aaslp_core/scripts/clr_nodes.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @file
+ * Contains clr_nodes.php.
+ *
+ * Clears node bundles for aaslp.lib.unb.ca.
+ */
+
+use Drupal\node\Entity\Node;
+
+clr_nodes('legal_article');
+
+/**
+ * Clear all nodes from a given bundle.
+ *
+ * @param string $bundle
+ *   A string indicating the bundle to update.
+ */
+function clr_nodes(string $bundle) {
+
+  $nids = \Drupal::entityQuery('node')
+    ->condition('type', $bundle)
+    ->execute();
+
+  if (!empty($nids)) {
+    foreach ($nids as $nid) {
+      $node = Node::load($nid);
+
+      if ($node) {
+        $title = $node->getTitle();
+        $node->delete();
+        echo "[-] [$title]->[DELETED]\n";
+      }
+    }
+  }
+}
