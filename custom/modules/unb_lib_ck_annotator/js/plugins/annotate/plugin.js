aa802675e3cf58e62ad87a4329d49b3c9e4526cb
camilocodes <cvillami@unb.ca>
2021-07-14 16:16:15 -0300

    AASLP-109 Add brackets to widget

--- custom/modules/unb_lib_ck_annotator/js/plugins/annotate/plugin.js
+++ custom/modules/unb_lib_ck_annotator/js/plugins/annotate/plugin.js
@@ -4,28 +4,34 @@ CKEDITOR.plugins.add( 'annotate', {
   icons: 'annotate',
 
   init: function( editor ) {
+    // Make span tags editable in CKEDITOR dtd.
     CKEDITOR.dtd.$editable['span'] = 1;
 
+    // Add button.
     editor.ui.addButton( 'annotate', {
       label: 'Annotate',
       title: 'Insert annotation',
       command: 'annotate',
     });
 
+    // Register widget.
     editor.widgets.add( 'annotate', {
       button: 'Insert annotation',
 
+      // Define template.
       template:
-        '<span class="unb-lib-anno">&nbsp;' +
+        '<span class="unb-lib-anno">&nbsp;[' +
             '<span class="anno-body">Annotation text...</span>' +
-        '&nbsp;</span>',
+        ']&nbsp;</span>',
 
+        // Define editable elements.
         editables: {
           content: {
               selector: '.anno-body'
           }
       },
 
+      // Function needs to return the outermost element for widget persistence.
       upcast: function( element ) {
           return element.name == 'span' && element.hasClass( 'unb-lib-anno' );
       }
