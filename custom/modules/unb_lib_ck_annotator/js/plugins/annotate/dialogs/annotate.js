be4d57676696250a48e492ad4c96fe80edee4239
camilocodes <cvillami@unb.ca>
2021-07-15 15:43:06 -0300

    AASLP-109 Add persistent widget dialog values

--- custom/modules/unb_lib_ck_annotator/js/plugins/annotate/dialogs/annotate.js
+++ custom/modules/unb_lib_ck_annotator/js/plugins/annotate/dialogs/annotate.js
@@ -13,16 +13,30 @@ CKEDITOR.dialog.add( 'annotate', function(editor) {
             type: 'select',
             label: 'Group',
             items: [
-              ['Numbers', 0],
-              ['Lowercase', 1],
-              ['Uppercase', 2]
-            ]
+              ['Numbers', 'num'],
+              ['Lowercase', 'lower'],
+              ['Uppercase', 'upper']
+            ],
+
+            setup: function(widget) {
+              this.setValue(widget.data.group);
+            },
+            commit: function(widget) {
+              widget.setData('group', this.getValue());
+            }
           },
           {
             id: 'body',
             type: 'text',
             label: 'Annotation text',
-            width: '380px'
+            width: '380px',
+
+            setup: function(widget) {
+              this.setValue(widget.data.body);
+            },
+            commit: function(widget) {
+              widget.setData('body', this.getValue());
+            }
           }
         ]
       }
