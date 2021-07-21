3a3b533857e20289124041830240713edafd1482
camilocodes <cvillami@unb.ca>
2021-07-21 16:07:30 -0300

    AASLP-109 Load configuration into annotator php plugin

--- custom/modules/unb_lib_ck_annotator/src/Plugin/CKEditorPlugin/Annotate.php
+++ custom/modules/unb_lib_ck_annotator/src/Plugin/CKEditorPlugin/Annotate.php
@@ -55,6 +55,30 @@ class Annotate extends CKEditorPluginBase implements CKEditorPluginCssInterface 
    * {@inheritdoc}
    */
   public function getConfig(Editor $editor) {
+    // Get immutable Config (Read Only).
+    $groups = \Drupal::config('unb_lib_ck_annotator.settings')->get('groups');
+    // Prepare configuration for plugin.
+    $config['groups'] = [
+      'numbered' => [
+        'enabled' => $groups['numbered']['enabled']
+        ],
+      'lowercase' => [
+        'enabled' => $groups['lowercase']['enabled']
+        ],
+      'uppercase' => [
+        'enabled' => $groups['uppercase']['enabled']
+        ],
+      'numbered' => [
+        'label' => $groups['numbered']['label']
+        ],
+      'lowercase' => [
+        'label' => $groups['lowercase']['label']
+        ],
+      'uppercase' => [
+        'label' => $groups['uppercase']['label']
+        ]
+    ];
+
     return [];
   }
 
