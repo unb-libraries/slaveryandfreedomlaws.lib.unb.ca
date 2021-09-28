5b521d668d790a69dc62894c9bfe36c878db5031
cvillami <cvillami@unb.ca>
2021-09-28 15:17:47 -0300

    AASLP-85 Add script to correct system.schema entries

---
+++ custom/modules/aaslp_core/scripts/clr_sys_schema.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains clr_sys_schema.php.
+ *
+ * Clears system.schema entries for aaslp.lib.unb.ca.
+ */
+
+clr_sys_schema('sources');
+clr_sys_schema('devel');
+
+/**
+ * Clear all system.schema entries matching name.
+ *
+ * @param string $name
+ *   A string indicating the entry name.
+ */
+function clr_sys_schema(string $name) {
+  \Drupal::service('database')->delete('key_value')->condition('name', $name)->execute();
+}
