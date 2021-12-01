55312301550e918517a13d7071abb6cc7a2c3557
camilocodes <cvillami@unb.ca>
2021-12-01 13:43:06 -0400

    AASLP-135 Add JS to force correct query on [Enter] for homepage search

--- custom/modules/aaslp_core/js/homepage-active-tabs.js
+++ custom/modules/aaslp_core/js/homepage-active-tabs.js
@@ -1,17 +1,31 @@
 (function($) {
   'use strict';
 
-  // Ensure that tab titles are active when their content is.
   Drupal.behaviors.homepageActiveTabs = {
     attach: function (context, settings) {
       // On documeny ready.
       $(document).ready( function() {
+        // Ensure that tab titles are active when their content is.
         if ($('#title').hasClass('active')) {
           $('#tab-title').addClass('active');
         }
         if ($('#fulltext').hasClass('active')) {
           $('#tab-fulltext').addClass('active');
         }
+      });
+
+      // Autofocus the search input of clicked tab.
+      $('.nav-tabs a[data-toggle="tab"]').on('shown.bs.tab', function() {
+          $('#aaslp-core-homepage input[type="text"]:visible').focus();
+      });
+
+      // Simulate click function when user presses enter key.
+      $('input[type="text"]').on('keypress', function (e) {
+          if (e.keyCode == 13) {
+              $('#aaslp-core-homepage input[type=submit]:visible').focus();
+              $('#aaslp-core-homepage input[type=submit]:visible').click();
+              return false;
+          }
       });
     },
   };
