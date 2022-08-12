668d9340031ae9f6f4c5ae614a9abacb1a1dd154
Patrick Schilf <pschilf@unb.ca>
2022-08-12 14:45:20 -0300

    TESTING-1 Migrate aaslp

---
+++ tests/cypress/status/e2e/aaslp.cy.js
@@ -0,0 +1,27 @@
+const host = 'https://aaslp.lib.unb.ca'
+describe('Anglo-Atlantic Slave Law Project', {baseUrl: host, groups: ['sites']}, () => {
+  context('Front page', {baseUrl: host}, () => {
+    beforeEach(() => {
+      cy.visit('/')
+      cy.title()
+        .should('contain', 'The Anglo-Atlantic Slave Law Project')
+    })
+
+    specify('Submitting "Title Search" query should lead to "laws" page', () => {
+      cy.get('form#aaslp-core-homepage')
+        .submit()
+      cy.url()
+        .should('match', /\/laws/)
+      cy.get('h1')
+        .should('contain', 'Title Search')
+    })
+
+    specify('Navigation menu should contain "Timeline" link', () => {
+      cy.get('nav[role="navigation"] a')
+        .contains('Timeline')
+        .its('0.href')
+        .should('match', /\/timeline/)
+    })
+  })
+
+})
