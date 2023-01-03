2cc14145a5aa30ee1e0b55398e87d9f4ed0122a7
Patrick Schilf <pschilf@unb.ca>
2023-01-03 15:46:37 -0400

    Test creation of legal articles

--- tests/einbaum/e2e/aaslp.cy.js
+++ tests/einbaum/e2e/aaslp.cy.js
@@ -1,10 +1,19 @@
 describe('Anglo-Atlantic Slave Law Project', () => {
-  context('Front page', () => {
-    it('should contain a title', () => {
-      cy.visit('/')
-      cy.title()
-        .should('contain', 'The Anglo-Atlantic Slave Law Project')
-    })
+  Cypress.Workflows.run('createEntity', {
+    entityType: 'node',
+    users: {
+      authorized: [
+        'aaslp_editor',
+      ],
+      unauthorized: [
+        'user',
+      ],
+    },
+    formUrl: '/node/add/legal_article',
+    formData: {
+      title: 'An Act for settling and regulating the Creation of Legal Articles',
+    },
+    successMessage: 'Legal Article An Act for settling and regulating the Creation of Legal Articles has been created.',
+    successUrl: '/node/[0-9]+',
   })
-
 })
