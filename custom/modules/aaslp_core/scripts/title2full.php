0f059d803b1d7ddf27fa7d0f969db4c7db0978c2
Camilo Villamizar <cvillami@unb.ca>
2023-04-26 13:53:39 -0300

    FREEDOM-184 Title migration script + disable in form + generate hook

---
+++ custom/modules/aaslp_core/scripts/title2full.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @file
+ * Contains title2full.php.
+ *
+ * Copies title to full_title field when blank in aaslp.lib.unb.ca.
+ */
+
+use Drupal\node\Entity\Node;
+
+refresh_nodes('legal_article');
+
+/**
+ * Loads and saves all nodes from a given bundle.
+ *
+ * @param string $bundle
+ *   A string indicating the bundle to update.
+ */
+function refresh_nodes(string $bundle) {
+
+  $nids = \Drupal::entityQuery('node')
+    ->condition('type', $bundle)
+    ->execute();
+
+  $i = 0;
+
+  if (!empty($nids)) {
+    foreach ($nids as $nid) {
+      $node = Node::load($nid);
+
+      if ($node and empty($node->get('field_full_title')->getValue()[0]['value'])) {
+        $title = $node->getTitle();
+        $node->set('field_full_title', $title);
+        $node->save();
+        $full = $node->get('field_full_title')->getValue()[0]['value'];
+        echo "[$i][+] [$full]->[Updated]\n";
+        $i++;
+      }
+    }
+
+    echo "\n$i records updated\n";
+  }
+}
